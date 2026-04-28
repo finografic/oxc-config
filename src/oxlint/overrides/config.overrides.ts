ce111f255eee0964ce95615d3be79353f0181c78
Justin <justin.blair.rankin@gmail.com>
2026-04-28 09:02:01 +0200

    chore: addition to config overrides

--- src/oxlint/overrides/config.overrides.ts
+++ src/oxlint/overrides/config.overrides.ts
@@ -1,7 +1,7 @@
 import type { OxlintOverride } from 'oxlint';
 
 export const configOverrides: OxlintOverride = {
-  files: ['oxlint.config.ts', 'oxfmt.config.ts', 'vitest.config.ts', 'vite.config.ts'],
+  files: ['oxlint.config.ts', 'oxfmt.config.ts', 'vitest.config.ts', 'vite.config.ts', 'astro.config.ts'],
   rules: {
     'import/no-default-export': 'off',
   },
